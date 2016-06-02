cef5cd5cc82bdbdea610acfca59ccde28a009261
meerzaheen_90 <meerzaheen_90@hotmail.com>
2016-06-02 16:07:41 -0400

    added comments/revision, tried to make display fancy, finished final copy

--- COMP123-TheHeroClassAssignment/Hero.cs
+++ COMP123-TheHeroClassAssignment/Hero.cs
@@ -4,6 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+* COMP123-The Hero Class Assignment
+* Author: Meer Zaheen Nazmul
+* Student ID: 300522487
+* Date Last Modified: June 2nd 2016
+* Program Description: Using MS Visual Studio, Build and Implement a Hero Class
+* GitHub Link: https://github.com/MeerZaheen/COMP123-TheHeroClassAssignment.git
+*
+* Revision History:
+* a) Initial commit to add default .gitIgnore and .gitAttribute files.
+*       Date: May 31st, 2016, 8:27pm
+* b) Initial Commit
+*       Date: May 31st, 2016
+* c) Part a complete
+*       Date: May 31st 2016, 8:40pm
+* d) Part 1b, c, d done
+*       Date: May 31st 2016, 9:30pm
+* e) working on 1e
+*       Date: May 31st, 2016, 9:30pm
+* 
+*/
 namespace COMP123_TheHeroClassAssignment
 {
     /**
@@ -132,7 +153,7 @@ namespace COMP123_TheHeroClassAssignment
         public Hero(string name)
         {
             this._name = name;
-            _generateAbilities();
+            generateAbilities();
         }
 
         /**
@@ -146,16 +167,99 @@ namespace COMP123_TheHeroClassAssignment
         * @param {int} Health
         * @param {int} Speed
         */
-        private void _generateAbilities()
+        private void generateAbilities()
         {
             Random numbers = new Random();
-            this._strength = numbers.Next(1, 100);
-            this._health = numbers.Next(1, 100);
-            this._speed = numbers.Next(1, 100); 
+            this._strength = numbers.Next(1, 101); // random number between 1 and 100
+            this._health = numbers.Next(1, 101);   // random number between 1 and 100
+            this._speed = numbers.Next(1, 101);    // random number between 1 and 100
+        }
+
+        /**
+        * <summary>
+        * This method randomly determines if the Hero hits (20% of the time)
+        * </summary>
+        *
+        * @method _hitAttempt
+        * @returns {bool}
+        */
+
+        private bool _hitAttempt()
+        {
+            Random integer = new Random();
+            if (integer.Next(1, 6) == 2) // random number between 1 and 5
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        /**
+        * <summary>
+        * This method calculates the damage the Hero causes to the target on a 20% hit by a number between 1 and 6
+        * </summary>
+        *
+        * @method _hitDamage
+        * @returns {int} damage
+        */
+        private int _hitDamage()
+        {
+            Random value = new Random();
+            int damage = value.Next(1, 7) * _strength; // creates a random number between 1 and 6
+            return damage;
         }
 
         // PUBLIC METHODS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+        
+        
+        /**
+        * <summary>
+        * This method calls the private _hitAttempt value based on _hitDamage IF the hero was able to successfully
+        * hit the target based on 20%
+        * </summary>
+        *
+        * @method Fight
+        * @returns {void}
+        */
+        public void Fight()
+        {
+            Console.WriteLine("||||Attempting to attack.....||||");
+            Console.WriteLine();
+                if (_hitAttempt() == true)
+                {
+                    Console.WriteLine("successful hit!! {0} damage was dealth", _hitDamage());
+                }
+                else
+                {
+                    Console.WriteLine("Sorry, you missed. Try again");
+                }
+        }
 
+        /**
+         * <summary>
+         * This method printsHeros attributes on console
+         * </summary>
+         * 
+         * @method Show
+         * @returns {void}
+         */
+        public void Show()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("||||||- Hero's Attributes- ||||||");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("||Properties||          ||Value||");
+            Console.WriteLine("--------------          ---------");
+            Console.WriteLine("||Strength        |       ||{0}||", _strength);
+            Console.WriteLine("||Speed           |       ||{0}||", _speed);
+            Console.WriteLine("||Health          |       ||{0}||", _health);
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine();
+        }
 
     }
 }
--- COMP123-TheHeroClassAssignment/Program.cs
+++ COMP123-TheHeroClassAssignment/Program.cs
@@ -4,12 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/**
+* COMP123-The Hero Class Assignment
+* Author: Meer Zaheen Nazmul
+* Student ID: 300522487
+* Date Last Modified: June 2nd 2016
+* Program Description: Using MS Visual Studio, Build and Implement a Hero Class
+* GitHub Link: https://github.com/MeerZaheen/COMP123-TheHeroClassAssignment.git
+*
+* Revision History:
+* a) Initial commit to add default .gitIgnore and .gitAttribute files.
+*       Date: May 31st, 2016, 8:27pm
+* b) Initial Commit
+*       Date: May 31st, 2016
+* c) Part a complete
+*       Date: May 31st 2016, 8:40pm
+* d) Part 1b, c, d done
+*       Date: May 31st 2016, 9:30pm
+* e) working on 1e
+*       Date: May 31st, 2016, 9:30pm
+* 
+*/
+
 namespace COMP123_TheHeroClassAssignment
 {
-    class Program
+    /**
+    * <summary>
+    * This is the "DRIVER" for the Hero program
+    * </summary>
+    *
+    * @class Program
+    */
+
+    public class Program
     {
-        static void Main(string[] args)
+        /**
+        * <summary>
+        * This is the main method for our Hero class plus a do/while loop for the to play again if wanted
+        * </summary>
+        *
+        * @Method Main
+        * @param {string[]} args
+        */
+
+        public static void Main(string[] args)
         {
+            // Create a new instance for the Hero Class
+            string playerReply = " ";
+                Hero Meer = new Hero("Meer");
+            do
+            {
+                Console.WriteLine();
+                Meer.Show();
+                Console.WriteLine();
+                Meer.Fight();
+                Console.WriteLine();
+                Console.WriteLine("Type 'yes' to play again!");
+                Console.WriteLine();
+                playerReply = Console.ReadLine();
+            } while (playerReply == "yes");
         }
     }
 }
